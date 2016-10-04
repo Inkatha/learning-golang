e1a3063f234264d7d1522c2445a9f688e302070e
minkatha01@gmail.com <minkatha01@gmail.com>
2016-10-04 15:28:08 -0500

    Add section for functions

--- pluralsight/functions/basics/func.go
+++ pluralsight/functions/basics/func.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	module := "function basics"
+	author := "nigel pulton"
+
+	fmt.Println(converter(module, author))
+}
+
+func converter(s1, s2 string) (s1, s2 string) {
+	s1 = strings.Title(s1)
+	s2 = strings.ToUpper(s2)
+
+	return s1, s2
+}
